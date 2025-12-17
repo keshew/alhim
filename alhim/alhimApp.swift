47534691e39040aca7853bd940c7cfbd3dde248e
keshew <artyom.korotckov2017@yandex.ru>
2025-12-17 17:17:19 +0500

    Initial Commit

---
+++ alhim/alhimApp.swift
@@ -0,0 +1,17 @@
+//
+//  alhimApp.swift
+//  alhim
+//
+//  Created by Артём Коротков on 17.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct alhimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
